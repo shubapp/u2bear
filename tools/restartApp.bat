42deef5a2733b10853b90c6e8175a483d049f1ea
Dean Shub <deanshub@gmail.com>
2014-07-26 10:14:00 +0300

    fixes #9 updater not deleting videos anymore

--- tools/restartApp.bat
+++ tools/restartApp.bat
@@ -1,4 +1,7 @@
 timeout /t 5 /nobreak
-rd /S /Q %1
+del /F /Q %1\*
+pause
+for /D /R %1 %%a in (*) do (if /I NOT '%%a' == '%1\videos' (if /I NOT '%%a' == '%1\images' (if /I NOT '%%a' == '%1\songs' (rd /S /Q %%a))))
+pause
 xcopy /E /I /Y %2 %1
 call "%~1\%~3"
